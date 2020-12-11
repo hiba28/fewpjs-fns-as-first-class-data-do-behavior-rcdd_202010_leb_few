7bbf3a7f921f8eee3261168f1f32c5073241a8a2
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-11 04:40:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function handleClick(e) {
 function greet() {
   let d = new Date
   let hour = d.getHours();
-   var ampm = hour >= 12 ? 'pm' : 'am';
+  let ampm = hour >= 12 ? 'pm' : 'am';
   if(hour < 12){
     return "Good Morning";
   }
