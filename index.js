4a6f496cb47173e87fb15ee5adc1b39355923ef5
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-11 04:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function greet(string) {
   if(hour < 12){
     return "Good Morning";
   }
-  else if(hour > 12 && hour <5){
+  else if(hour >= 12 && hour <= 5){
     return "Good Afternoon";
   }
   else if(hour > 5){
