08a2b5f7c4d78e14dd892ec444e4b5e0ea1e1e49
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-11 04:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let hour = string.parseInt();
+  if(string <12){
+    return "Good Morning";
+  }
+  else if(string > 12 && string <5){
+    return "Good Afternoon";
+  }
+  else if(string > 5){
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let testContent = "TEST";
+  displayMessage(testContent);
+  let content = document.getElementById("greeting").innerText;
+}
